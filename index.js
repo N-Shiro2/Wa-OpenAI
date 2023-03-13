a8cf30bc2191aba498f8f91dc5b2f15c3d372cef
Nfal <68472628+N-Shiro2@users.noreply.github.com>
2023-03-13 20:29:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const sessionName = "yusril";
-const donet = "https://saweria.co/sansekai";
-const owner = ["6287878817169"]; // Put your number here ex: ["62xxxxxxxxx"]
+const sessionName = "nfal";
+const donet = "https://saweria.co/nfall";
+const owner = ["6281329307088"]; // Put your number here ex: ["62xxxxxxxxx"]
 const {
   default: sansekaiConnect,
   useMultiFileAuthState,
